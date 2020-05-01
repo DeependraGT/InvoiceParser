0b233f3b0d12766150041761058da08cffb8a0e3
Deependra Singh <deependra578@gmail.com>
2020-05-01 22:59:21 +0530

    Create zoom.js

---
+++ zoom.js
@@ -0,0 +1,17 @@
+function zoomIn(event) {
+  var pre = document.getElementById("preview");
+  pre.style.visibility = "visible";
+  if ($("#zoom1:hover").length != 0) {
+        var img = document.getElementById("zoom1");
+		pre.style.backgroundImage = "url('pic1.jpg')";
+    }
+  
+  var posX = event.offsetX;
+  var posY = event.offsetY;
+  pre.style.backgroundPosition=(-posX*2.5)+"px "+(-posY*5.5)+"px";
+}
+
+function zoomOut() {
+  var pre = document.getElementById("preview");
+  pre.style.visibility = "hidden";
+}
